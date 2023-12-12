b9f3cf88ec06623dfb562bd35b43b93e6341f761
max-fletcher <amar.naam.machine@gmail.com>
2023-12-12 14:19:26 +0600

    Added an explanation for fetching/submitting data.

--- pages/testing_usefetch.vue
+++ pages/testing_usefetch.vue
@@ -20,7 +20,17 @@
 </template>
 
 <script setup lang="ts">
-  // The 2nd param is to transform the data received to a useable format that we can it inside template tags
+  // ***IMPORTANT: The difference between '$fetch', 'useFetch' and 'useAsyncData' is that '$fetch' is the barebones fetch uses ofetch(see docs). The problem is that using '$fetch' to GET data causes double data fetching,
+  // once on the server-side and once on the client-side because '$fetch' does not transfer state from the server-side to the client-side so its not recommended to be used with 'ssr : true' to fetch(GET) data for hydrating the
+  // page. Instead, its better to use 'useAsyncData' for fetching(GET) data. '$fetch' should be used only if you want to POST or PUT/PATCH data(i.e '$fetch' inside a fucntion that will be triggered on form submit etc.).
+  // 'useAsyncData' will make a request to the server and transfer that state/data to the client without making a request on the client-side on 'ssr : true', which saves an extra API call.
+  // It looks like this  ->  const { data, pending, error, refresh } = await 'useAsyncData'('mountains', () => '$fetch'('https://api.nuxtjs.dev/mountains'))
+  // It is worth mentioning that the the 1st param of 'useAsyncData' is a key that is used to de-duplicate and trigger refresh/refetch this API call if need be, but as 2nd param, you can use '$fetch' or anything
+  // else such as axios to make the API call. There are optional 3rd(route params as object), 4th(watcher variables as array) and other params, but those are optional. 
+  // Also, you don't need to define async anywhere when using 'useAsyncData' because it is an async composable. Only use an await before the composable(i.e await 'useAsyncData'('...', () => '$fetch'('...'))).
+  // Finally, 'useAsyncData' is just the same as using 'useFetch' with '$fetch'. Its a shorthand and nothing more.
+
+  // The 2nd param is to transform the data received to a useable format that we can use inside template tags
   const {data : products} = await useFetch('/api/products', {
     transform: (_products) => _products.data
   });
